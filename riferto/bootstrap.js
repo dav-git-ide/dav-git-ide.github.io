6de67f8ae4804cd36a7862bd880c707e1ae3bc18
davide <54133498+dav-git-ide@users.noreply.github.com>
2026-08-29 20:13:59 +0200

    Prepare Riferto v0.10.5

--- riferto/bootstrap.js
+++ riferto/bootstrap.js
@@ -1,4 +1,4 @@
-const APP_VERSION='0.10.4';
+const APP_VERSION='0.10.5';
 const DB_NAME='riferto-db';
 const DB_VERSION=1;
 const isStandalone=window.matchMedia('(display-mode: standalone)').matches||navigator.standalone===true;
