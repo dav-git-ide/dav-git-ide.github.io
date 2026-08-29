9a67398775cf0faf63b67257641cc5e85321fc82
davide <54133498+dav-git-ide@users.noreply.github.com>
2026-08-29 21:59:12 +0200

    Bump Riferto to v0.10.11

--- riferto/bootstrap.js
+++ riferto/bootstrap.js
@@ -1,4 +1,4 @@
-const APP_VERSION='0.10.10';
+const APP_VERSION='0.10.11';
 const DB_NAME='riferto-db';
 const DB_VERSION=1;
 const isStandalone=window.matchMedia('(display-mode: standalone)').matches||navigator.standalone===true;
