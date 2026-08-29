5a4691ba986a983482af705679c7b877f724008b
davide <54133498+dav-git-ide@users.noreply.github.com>
2026-08-29 19:47:38 +0200

    Align Riferto bootstrap to v0.10.1

--- riferto/bootstrap.js
+++ riferto/bootstrap.js
@@ -1,4 +1,4 @@
-const APP_VERSION='0.10.0';
+const APP_VERSION='0.10.1';
 const DB_NAME='riferto-db';
 const DB_VERSION=1;
 const isStandalone=window.matchMedia('(display-mode: standalone)').matches||navigator.standalone===true;
