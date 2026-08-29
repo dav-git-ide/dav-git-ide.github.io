7648e69abf3bc5de6d048c413e14576969ec2468
davide <54133498+dav-git-ide@users.noreply.github.com>
2026-08-29 17:55:34 +0200

    Bump PWA cache for encrypted vault release

--- blood-vault/sw.js
+++ blood-vault/sw.js
@@ -1,4 +1,4 @@
-const CACHE='blood-vault-v1';
+const CACHE='blood-vault-v2';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./loinc-common.json'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
